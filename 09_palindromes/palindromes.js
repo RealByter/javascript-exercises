63dc7649d5e4c9cadc1cae956ef222713734cf84
RealByter <justavishay@gmail.com>
2023-03-04 14:10:10 +0200

    feat: complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = str => {
+	const newStr = str
+		.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+		.replaceAll(' ', '')
+		.toLowerCase();
+	return newStr === [...newStr].reverse().join('');
 };
 
 // Do not edit below this line
