b382b22b8445c912aea27592df24207fbe985530
RealByter <justavishay@gmail.com>
2023-03-04 16:01:03 +0200

    feat: complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = people =>
+	people.reduce(
+		(oldestPerson, currentPerson) => {
+			if (!('yearOfDeath' in currentPerson)) {
+				currentPersonYearOfDeath = new Date().getFullYear();
+			} else {
+				currentPersonYearOfDeath = currentPerson.yearOfDeath;
+			}
+			if (!('yearOfDeath' in oldestPerson)) {
+				oldestPersonYearOfDeath = new Date().getFullYear();
+			} else {
+				oldestPersonYearOfDeath = oldestPerson.yearOfDeath;
+			}
 
-};
+			const currentPersonAge =
+				currentPersonYearOfDeath - currentPerson.yearOfBirth;
+			const oldestPersonAge =
+				oldestPersonYearOfDeath - oldestPerson.yearOfBirth;
+			if (currentPersonAge > oldestPersonAge) {
+				return currentPerson;
+			} else {
+				return oldestPerson;
+			}
+		},
+		{ yearOfBirth: 2023, yearOfDeath: 2023 }
+	);
 
 // Do not edit below this line
 module.exports = findTheOldest;
