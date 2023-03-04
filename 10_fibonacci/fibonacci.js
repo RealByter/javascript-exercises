4c663d17671d936e5b9cad907a6594d49404dc6f
RealByter <justavishay@gmail.com>
2023-03-04 15:50:19 +0200

    feat: complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = size => {
+	if (size < 0) {
+		return 'OOPS';
+	}
 
+	let current = 1;
+	let previous = 1;
+	for (let i = 2; i < size; i++) {
+		const temp = current;
+		current += previous;
+		previous = temp;
+	}
+	return current;
 };
 
 // Do not edit below this line
