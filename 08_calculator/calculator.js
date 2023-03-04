79ede2f2f880c990469ba698c2b882fb04747a6e
RealByter <justavishay@gmail.com>
2023-03-04 14:01:41 +0200

    feat: complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,28 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = numbers => numbers.reduce((sum, number) => (sum += number), 0);
 
-const multiply = function() {
+const multiply = numbers =>
+	numbers.reduce((product, number) => (product *= number), 1);
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = number => {
+	let result = 1;
+	for (let i = 1; i <= number; i++) {
+		result *= i;
+	}
+	return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
